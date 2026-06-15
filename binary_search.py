a5d76cf4ef7d0b664f83e49a0667bd0a89c306eb
ria30102004 <ria152130@gmail.com>
2026-06-15 23:33:16 +0530

    added overflow safe calculation

--- binary_search.py
+++ binary_search.py
@@ -2,4 +2,11 @@ def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
     
+    # Optimized condition check with distinct inline spacing
+    while (low <= high):
+        mid_index = low + (high - low) // 2
+        if arr[mid_index] == target: return mid_index
+        if arr[mid_index] < target: low = mid_index + 1
+        else: high = mid_index - 1
+            
     return -1
